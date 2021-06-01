8ac88b2af9f6b09da529a19bc1ff1de1972cbee6
Srivathsa KM <31186404+SrivathsaKM@users.noreply.github.com>
2021-06-01 22:38:56 +0530

    Update ShowUser.js
    replaced multiple axios with single axios.all (promise.all)

--- ShowUser.js
+++ ShowUser.js
@@ -11,31 +11,53 @@ const ShowUser = (props) => {
   const [singleUser, setSingleUser] = useState({});
   const [singleUserPosts, setSingleUsersPosts] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then((response) => {
-        const result = response.data;
-        setSingleUser(result);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.log(error.message);
-      });
-  }, [id]);
+  const requestFirstId = axios.get(`https://jsonplaceholder.typicode.com/users/${id}`);
+  const requestSecondId = axios.get(`https://jsonplaceholder.typicode.com/posts?userId=${id}`);
 
   useEffect(() => {
     axios
-      .get(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
-      .then((response) => {
-        const result = response.data;
-        setSingleUsersPosts(result);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.log(error.message);
+      .all([requestFirstId, requestSecondId])
+      .then(
+        axios.spread((...response) => {
+          const resultOne = response[0].data;
+          const resultSecond = response[1].data;
+          //console.log(resultOne);
+          setSingleUser(resultOne);
+          setSingleUsersPosts(resultSecond);
+          setLoading(false);
+        })
+      )
+      .catch((errors) => {
+        console.log(errors.message);
+        setLoading(true);
       });
   }, [id]);
+
+  //   useEffect(() => {
+  //     axios
+  //       .get(`https://jsonplaceholder.typicode.com/users/${id}`)
+  //       .then((response) => {
+  //         const result = response.data;
+  //         setSingleUser(result);
+  //         setLoading(false);
+  //       })
+  //       .catch((error) => {
+  //         console.log(error.message);
+  //       });
+  //   }, [id]);
+
+  //   useEffect(() => {
+  //     axios
+  //       .get(`https://jsonplaceholder.typicode.com/posts?userId=${id}`)
+  //       .then((response) => {
+  //         const result = response.data;
+  //         setSingleUsersPosts(result);
+  //         setLoading(false);
+  //       })
+  //       .catch((error) => {
+  //         console.log(error.message);
+  //       });
+  //   }, [id]);
 
   return (
     <div>
